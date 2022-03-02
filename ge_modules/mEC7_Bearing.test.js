c1c7cac1b96c1648ca70c3d6decdd74d0d4340ff
simon thomson <simon.thomson@gmx.co.uk>
2022-03-02 11:31:47 +0000

    test added

--- ge_modules/mEC7_Bearing.test.js
+++ ge_modules/mEC7_Bearing.test.js
@@ -1,36 +1,36 @@
 
 
-// const mEC7Bearing = require('../ge_modules/mEC7_Bearing');
+const mEC7Bearing = require('../ge_modules/mEC7_Bearing');
 
-// // const getAboutUsLink = require("./index");
+// const getAboutUsLink = require("./index");
 
-// // test("Returns about-us for english language", () => {
-// //     expect(getAboutUsLink("en-US")).toBe("/about-us");
-// // });
-
-// const d2_data1 = {
-//                 length: 6,
-//                 breadth: 3,
-//                 hload:0,
-//                 area:18,
-//                 alpha_rad:0,
-//                 cu:75
-// 
-
-// const d2_resp1 = {
-//                 length: 6,
-//                 breadth: 3,
-//                 area:18,
-//                 alpha_rad:0,
-//                 cu:75,
-//                 nc:5.14159, 
-//                 bc:1,
-//                 sc:1.1,
-//                 ic:1,
-//                 q_nc: 424.181175
-
-// }
-
-// test("Check D2", () => {
-//     expect(mEC7Bearing.calc_EC7_D2_data(d2_data1)).toBe(d2_resp1);
+// test("Returns about-us for english language", () => {
+//     expect(getAboutUsLink("en-US")).toBe("/about-us");
 // });
+
+const d2_data1 = {
+                length: 6,
+                breadth: 3,
+                hload:0,
+                area:18,
+                alpha_rad:0,
+                cu:75
+
+}
+const d2_resp1 = {
+                length: 6,
+                breadth: 3,
+                area:18,
+                alpha_rad:0,
+                cu:75,
+                nc:5.14159, 
+                bc:1,
+                sc:1.1,
+                ic:1,
+                q_nc: 424.181175
+
+}
+
+test("Check D2", () => {
+    expect(mEC7Bearing.calc_EC7_D2_data(d2_data1)).toBe(d2_resp1);
+});
